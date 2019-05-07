aa7f2ecd74741d20f66b8d30be9f02134e7cf22d
Alex Doss <alexdoss@Alexs-MacBook-Pro-5.local>
2019-05-07 17:39:01 -0600

    Adds username to chat

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -21,7 +21,7 @@ class Chat extends Component {
   addMessage = (data) => {
     const newConvo = this.state.conversation.slice()
     console.log(newConvo)
-    newConvo.push(data.message)
+    newConvo.push(data)
     this.setState({
       conversation: newConvo
     })
@@ -46,11 +46,19 @@ class Chat extends Component {
     return (
       <div id="adventure-chat">
         <div id="chat-window">
-          <div id="output">{ conversation.map((message) => {
-            return (<p>{message}</p>)
+          <div id="output">{ conversation.map((data) => {
+            return (<p><em>{data.username}</em>: {data.message}</p>)
           }) }</div>
         </div>
         
+        <input 
+        id='username' 
+        name='username' 
+        type="text" 
+        placeholder="Username" 
+        value={ this.state.username }
+        onChange = { this.handleOnChange }/>
+
         <input 
         id='message' 
         name='message' 
